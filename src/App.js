9074fc512ab97152cb95a3d6279be422455bfd5e
Lakshay Agarwal <lakshay.agar@gmail.com>
2021-08-27 10:26:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Connection from './components/Connection';
+
+function App() {
+  return (
+    <div className="App">
+     <Connection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Connection.js
@@ -0,0 +1,14 @@
+const Connection =()=>{
+    return(
+        <nav className = 'navbar'>
+            <h1>Lakshay Agarwal</h1>
+            <div className='links'>
+                <a href=""> Home </a>
+                <a href="" href=""> About Me </a>
+                <a href="" >Courses</a>
+                <a href="">Tell me Something</a>
+            </div>
+        </nav>)
+}
+
+export default Connection;
